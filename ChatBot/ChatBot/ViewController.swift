97ce3d8d2807e27f81aaf47fe71bb2e1413c155e
AlessandroTaglieri <taglieri96@gmail.com>
2019-03-06 17:32:26 +0100

    Upload empty project

---
+++ ChatBot/ChatBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatBot
+//
+//  Created by Digital First on 06/03/19.
+//  Copyright © 2019 Digital First. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
